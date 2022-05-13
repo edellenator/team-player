004f60869340b4ec9653476b380c671ed8bd5a11
Erik Dell <edellenator@gmail.com>
2022-05-12 21:23:13 -0700

    html base template

---
+++ src/page-template.js
@@ -0,0 +1,32 @@
+
+
+const generateEmployeeCards (name, id, email, officeNumber, github, school) => {
+    
+}
+
+
+module.exports = templateData => {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="display-2 text-center mb-4 p-5 bg-warning">
+        My Team
+    </header>
+    <section>
+        <div id="employee-cards" class="container"></div>
+    </section>
+</body>  
+  `
+}
